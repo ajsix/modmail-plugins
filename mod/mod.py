00aa163092eca95eb4fddfecd448f8ad2c2bfeb5
zvxjwaa <70423086+ajsix@users.noreply.github.com>
2020-10-26 15:09:18 +0800

    Update and rename moderation.py to mod.py

--- mod/mod.py
+++ mod/mod.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-class moderation(commands.Cog):
+class mod(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
